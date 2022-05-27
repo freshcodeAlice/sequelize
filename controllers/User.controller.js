8d149cc6ec2a9cadd55dd1fafecfbe607bca7cde
freshcodeAlice <freshjs.ofte@gmail.com>
2022-05-27 19:59:05 +0300

    Done!

--- controllers/User.controller.js
+++ controllers/User.controller.js
@@ -4,6 +4,62 @@ module.exports.createUser = async (req, res, next) => {
   try {
     const { body } = req;
     const createdUser = await User.create(body);
-    res.status(201).send(createdUser);
-  } catch (error) {}
+    res.status(201).send({ data: createdUser });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.getAllUsers = async (req, res, next) => {
+  try {
+    const arrayUsers = await User.findAll();
+    res.status(200).send({ data: arrayUsers });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.getUser = async (req, res, next) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    const returnedUser = await User.findByPK(id);
+    res.status(200).send({ data: returnedUser });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.updateUser = async (req, res, next) => {
+  const {
+    body,
+    params: { id }
+  } = req;
+  try {
+    const updatedUser = await User.update(body, {
+      where: {
+        id: id
+      }
+    });
+    res.status(200).send({ data: updatedUser });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.deleteUser = async (req, res, next) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    const deletedUser = await User.destroy({
+      where: {
+        id: id
+      }
+    });
+    res.status(200).send(deletedUser);
+  } catch (error) {
+    next(error);
+  }
 };
--- router.js
+++ router.js
@@ -4,5 +4,9 @@ const UserController = require('./controllers/User.controller');
 const router = Router();
 
 router.post('/user', UserController.createUser);
+router.get('/users', UserController.getAllUsers);
+router.get('/user/:id', UserController.getUser);
+router.delete('/user/:id', UserController.deleteUser);
+router.put('/user/:id', UserController.updateUser);
 
 module.exports = router;
