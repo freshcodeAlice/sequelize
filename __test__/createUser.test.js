31b15d2e38ec8a03c0f699117eff714c705ccc0f
freshcodeAlice <freshjs.ofte@gmail.com>
2022-06-30 17:35:20 +0300

    create user test

--- __test__/createUser.test.js
+++ __test__/createUser.test.js
@@ -1,9 +1,19 @@
 const app = require('../app');
 const request = require('supertest');
 const db = require('../models');
-
+const yup = require('yup');
 
 const appRequest = request(app);
+
+const bodySchema = yup.object({
+    data: yup.object({
+        firstName: yup.string().required(),
+        lastName: yup.string().required(),
+        email: yup.string().email().required(),
+        birthday: yup.date(),
+        password: yup.string(),
+    })
+});
 
 const getUser = () => ({
     firstName: "Test",
@@ -13,12 +23,20 @@ const getUser = () => ({
     password:"adwer42343"
 });
 
+
+
 const user = getUser();
 
 describe('create new user', () => {
     test('user create succesfully expect 201', async () => {
         const response = await appRequest.post('/api/users/').send(user);
         expect(response.statusCode).toBe(201);
-        db.sequelize.close();
+        expect(bodySchema.isValidSync(response.body)).toBe(true);
     })
 })
+
+
+
+afterAll(() => {
+    db.sequelize.close();
+  });
